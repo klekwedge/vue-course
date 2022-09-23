35357afb349eb3940fad448e5d2630def82c639d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-23 20:59:17 +0300

    feat: add 1 lesson

---
+++ 3. The concept of Single Source of Truth/01 Introduction to Vuex. State. Getters/src/components/HeaderComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Header
+    {{ usersList }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'HeaderComp',
+  computed: {
+    ...mapGetters('users', ['usersList']),
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ 3. The concept of Single Source of Truth/01 Introduction to Vuex. State. Getters/src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex';
+import users from './modules/users';
+
+export default createStore({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: { users },
+});
