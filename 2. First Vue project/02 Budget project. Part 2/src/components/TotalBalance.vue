910e16a0ce6231c0756f7fd27bb9e51392ddd481
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-23 13:40:21 +0300

    feat: add 2 lesson

---
+++ 2. First Vue project/02 Budget project. Part 2/src/components/TotalBalance.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="total-value">Balance: {{ total }}</div>
+</template>
+
+<script>
+export default {
+  name: 'TotalBalance',
+  props: {
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: () => ({}),
+};
+</script>
+
+<style scoped lang="scss">
+.total-value {
+  font-size: 26px;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+</style>
