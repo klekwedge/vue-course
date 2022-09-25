1b43c48ebced385b0826a13a09ed7e5e211183aa
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-25 11:55:26 +0300

    feat: add 3 lesson

---
+++ 3. The concept of Single Source of Truth/03 Vuex homework/src/components/TotalBalance.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="total-value" :class="[changeColor]">Balance: {{ totalBalance }}</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'TotalBalance',
+  computed: {
+    ...mapGetters('budget', ['totalBalance']),
+    changeColor() {
+      if (this.totalBalance > 0) {
+        return '_green';
+        // eslint-disable-next-line no-else-return
+      } else if (this.totalBalance === 0) {
+        return '';
+      } else {
+        return '_red';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.total-value {
+  font-size: 26px;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+
+._green {
+  color: #38a169;
+}
+
+._red {
+  color: #e53e3e;
+}
+</style>
